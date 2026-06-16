a3ee266ab9820ba953664438afe5fd68ba9c82f1
Christian Hopper <potatoslicer18@protonmail.com>
2026-06-15 20:32:40 -0400

    Delete CtblPlusPlus.Domain directory

--- CtblPlusPlus.Domain/Exceptions/DatabaseLockedException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace CtblPlusPlus.Core.Exceptions;
-
-public class DatabaseLockedException : Exception
-{
-    public DatabaseLockedException(string message, Exception innerException)
-        : base(message, innerException) { }
-}
