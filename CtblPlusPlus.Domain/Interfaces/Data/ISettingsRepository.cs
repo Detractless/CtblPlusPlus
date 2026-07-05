5db68c5766a76f0118ae1ffc6b221bc75f4ab678
Christian Darby-Ward <potatoslicer18@protonmail.com>
2026-07-05 10:57:06 -0400

    Delete CtblPlusPlus.Domain directory

--- CtblPlusPlus.Domain/Interfaces/Data/ISettingsRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace CtblPlusPlus.Core.Interfaces.Data;
-
-public interface ISettingsRepository
-{
-    string GetSetting(string key, string defaultValue = "");
-    void SetSetting(string key, string value);
-}
-
-
--- CtblPlusPlus.Domain/Interfaces/Identity/IAppIdentityResolver.cs
+++
@@ -1,9 +0,0 @@
-namespace CtblPlusPlus.Core.Interfaces.Identity;
-
-public interface IAppIdentityResolver
-{
-    string GetAppName(string exePath);
-    string GetPublisher(string exePath);
-    string GetFileHashSha256(string exePath);
-    (string AppName, string Publisher, string FileHashSha256) GetIdentity(string exePath);
-}
