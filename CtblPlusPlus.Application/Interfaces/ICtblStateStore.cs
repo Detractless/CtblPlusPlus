b86fd3a6e694f2ffc2283c9df5cd1d4a90f99739
Christian Darby-Ward <potatoslicer18@protonmail.com>
2026-07-05 10:59:32 -0400

    Delete CtblPlusPlus.Application directory

--- CtblPlusPlus.Application/Interfaces/ICtblStateStore.cs
+++
@@ -1,9 +0,0 @@
-using CtblPlusPlus.Core.Models;
-
-namespace CtblPlusPlus.Application.Interfaces;
-
-public interface ICtblStateStore
-{
-    CtblRoot GetDbState();
-    void SaveDbState(CtblRoot state);
-}
